350f44fbb5a3f85d47575c9cdec7933eb82e4b09
Pine Netcharussaeng <pinetcht@gmail.com>
2025-04-06 11:14:06 -0700

    committing after github.io

--- HackathonProject/frontend/housing47/src/pages/SignIn.jsx
+++ HackathonProject/frontend/housing47/src/pages/SignIn.jsx
@@ -60,7 +60,7 @@ export default function SignIn() {
     
     try {
       // Send POST request to your server
-      const response = await axios.post('http://localhost:5001/login', formData);
+      const response = await axios.post('https://backend-housing47.onrender.comlogin', formData);
       
       console.log("Login successful:", response.data);
       
